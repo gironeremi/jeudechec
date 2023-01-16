e08a9e1142bfe1921f9743b1b5b0894453141a03
GIRONE Remi <remi.girone@instant-system.com>
2023-01-16 11:18:32 +0100

    added pieces, and still trying to make a php-generated board

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # jeudechec
+Disposition des pièces:
+## blancs
+
+* a1=tour
+* b1=cavalier
+* c1=fou
+* d1=queen
+* e1=roi
+* f1=fou
+* g1=cavalier
+* h1=tour
+
+a2,b2, etc = pion
+
+## noirs
+a7,b7, etc = pion
+
+* a7=tour
+* b7=cavalier
+* c7=fou
+* d7=queen
+* e7=roi
+* f7=fou
+* g7=cavalier
+* h7=tour
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     </section>
     <div id="chessboard">
       <div class="row">
-        <div class="square white"><img src="public/media/chess-pawn-regular.svg"></div>
+        <div class="square white"><img src="public/media/chess_pieces/pawn-white.svg"></div>
         <div class="square black"></div>
         <div class="square white"></div>
         <div class="square black"></div>
@@ -95,6 +95,7 @@
       </div>
     </div>
     <?php
+      //création du plateau vide avec les coordonnées
       $chessboard = array();
       $letter = 'a';
       $number = 1;
@@ -107,10 +108,58 @@
               $number++;
           }
       }
-      $chessboard["a2"][0] = "pion";
-      $chessboard["b2"][0] = "pion";
-      $chessboard["a1"][0] = "Tour";
+      //ajout des pièces 
+      $chessboard["a2"] = "pawn-white.svg";
+      $chessboard["b2"] = "pawn-white.svg";
+      $chessboard["c2"] = "pawn-white.svg";
+      $chessboard["d2"] = "pawn-white.svg";
+      $chessboard["e2"] = "pawn-white.svg";
+      $chessboard["f2"] = "pawn-white.svg";
+      $chessboard["g2"] = "pawn-white.svg";
+      $chessboard["h2"] = "pawn-white.svg";
+      $chessboard["a1"] = "rook-white.svg";
+      $chessboard["b1"] = 'knight-white.svg';
+      $chessboard["c1"] = 'bishop-white.svg';
+      $chessboard["d1"] = 'queen-white.svg';
+      $chessboard["e1"] = 'king-white.svg';
+      $chessboard["f1"] = 'bishop-white.svg';
+      $chessboard["g1"] = 'knight-white.svg';
+      $chessboard["h1"] = 'rook-white.svg';
+      $chessboard["a7"] = 'pawn-black.svg';
+      $chessboard["b7"] = 'pawn-black.svg';
+      $chessboard["c7"] = 'pawn-black.svg';
+      $chessboard["d7"] = 'pawn-black.svg';
+      $chessboard["e7"] = 'pawn-black.svg';
+      $chessboard["f7"] = 'pawn-black.svg';
+      $chessboard["g7"] = 'pawn-black.svg';
+      $chessboard["h7"] = 'pawn-black.svg';
+      $chessboard["a8"] = 'rook-black.svg';
+      $chessboard["b8"] = 'knight-black.svg';
+      $chessboard["c8"] = 'bishop-black.svg';
+      $chessboard["d8"] = 'queen-black.svg';
+      $chessboard["e8"] = 'king-black.svg';
+      $chessboard["f8"] = 'bishop-black.svg';
+      $chessboard["g8"] = 'knight-black.svg';
+      $chessboard["h8"] = 'rook-black.svg';
       ?>
+      <div class="chessboard">
+      <?php
+        foreach ($chessboard as $cell) {
+          ?>
+        <div id="" class="square">
+        
+        <?php
+        if (!empty($cell)) {
+          ?>
+            <img src="public/media/chess_pieces/<?= $cell ?>" />
+          <?php
+        }
+        ?>
+        </div>
+          <?php
+        }
+      ?>
+      </div>
       <pre><?php var_dump($chessboard) ?></pre>
   <script type="text/javascript" src="public/js/canvas.js"></script>
   <script type="text/javascript" src="public/js/main.js"></script>
